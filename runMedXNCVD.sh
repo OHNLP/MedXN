81b703fa7e0ab889c4fbd8ebc9703312eba79ec7
james_masanz <james_masanz@86f73235-997c-406d-aa19-21ee2444f377>
2013-12-09 17:27:46 +0000

    Fix issue #118: MedXN scripts should not require lib/*. MedXN-1.0.jar does not contain all dependencies
    git-svn-id: https://svn.code.sf.net/p/ohnlp/code/trunk/MedXN@1124 86f73235-997c-406d-aa19-21ee2444f377

--- runMedXNCVD.sh
+++ runMedXNCVD.sh
@@ -1,2 +1,2 @@
-java -Xms512M -Xmx2048M -cp resources:desc:descsrc:medtaggerdescsrc:MedXN-1.0.1.jar:lib/* org.apache.uima.tools.cvd.CVD
+java -Xms512M -Xmx2048M -cp resources:desc:descsrc:medtaggerdescsrc:MedXN-1.0.1.jar org.apache.uima.tools.cvd.CVD
 
