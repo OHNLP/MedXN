cd918e68f01fdea94ed74df519c6a7bb71f04002
james_masanz <james_masanz@86f73235-997c-406d-aa19-21ee2444f377>
2013-12-09 17:08:26 +0000

    #117 Update MedXN release to 1.0.1 (build MedXN-1.0.1)
    git-svn-id: https://svn.code.sf.net/p/ohnlp/code/trunk/MedXN@1123 86f73235-997c-406d-aa19-21ee2444f377

--- runMedXNCPE.sh
+++ runMedXNCPE.sh
@@ -1,2 +1,2 @@
-java -Xms512M -Xmx2048M -cp resources:desc:descsrc:medtaggerdescsrc:MedXN-1.0.jar:lib/* org.apache.uima.tools.cpm.CpmFrame 
+java -Xms512M -Xmx2048M -cp resources:desc:descsrc:medtaggerdescsrc:MedXN-1.0.1.jar:lib/* org.apache.uima.tools.cpm.CpmFrame 
 
